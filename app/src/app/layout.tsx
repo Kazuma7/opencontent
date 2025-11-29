723c3f077ae36bc9db6b65600d9dae1fabf8f162
inaridiy <inainainariinainari@gmail.com>
2025-11-30 00:57:26 +0900

    chore: install wagmi

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,6 +3,10 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
+import { headers } from "next/headers";
+import { cookieToInitialState } from "wagmi";
+import { config as wagmiConfig } from "@/lib/wagmi";
+import { Providers } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,18 +23,26 @@ export const metadata: Metadata = {
   description: "OpenContent - デジタルコンテンツ販売プラットフォーム",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const headerList = await headers();
+  const cookie = headerList.get("cookie");
+
+  // 2. Cookie情報を基にWagmiの初期状態を復元
+  const initialState = cookieToInitialState(wagmiConfig, cookie);
+
   return (
     <html lang="ja">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex min-h-screen flex-col`}
       >
         <Header />
-        <main className="flex-1">{children}</main>
+        <Providers initialState={initialState}>
+          <main className="flex-1">{children}</main>
+        </Providers>
         <Footer />
       </body>
     </html>
---
+++ app/src/app/providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { FC, type ReactNode, useState } from "react";
+import { type State, WagmiProvider } from "wagmi";
+import { config } from "@/lib/wagmi";
+
+const queryClient = new QueryClient();
+
+export const Providers: FC<{
+  children: ReactNode;
+  initialState?: State;
+}> = ({ children, initialState }) => {
+  return (
+    <WagmiProvider config={config} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  );
+};
