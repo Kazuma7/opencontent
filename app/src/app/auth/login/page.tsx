a63e957174981cd936cb872ed7a3f60983ca56f9
inaridiy <inainainariinainari@gmail.com>
2025-11-30 19:12:24 +0900

    feat: lock

--- app/src/app/auth/login/page.tsx
+++ app/src/app/auth/login/page.tsx
@@ -1,5 +1,16 @@
-import { AuthLoginView } from "@/features/auth-login/views/AuthLoginView";
+import { redirect } from "next/navigation";
 
-export default function AuthLoginPage() {
+import { AuthLoginView } from "@/features/auth-login/views/AuthLoginView";
+import { getSession } from "@/lib/auth";
+
+export const revalidate = 0;
+
+export default async function AuthLoginPage() {
+  const session = await getSession();
+
+  if (session.data.sessions.length > 0) {
+    redirect("/");
+  }
+
   return <AuthLoginView />;
 }
